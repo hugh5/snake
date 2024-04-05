de2e140ef8ef26dc58e0246290c1dd3f743f01fe
hugh5 <hughdrummond11@gmail.com>
2024-04-06 04:56:14 +1000

    Initial Commit

---
+++ snake/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  snake
+//
+//  Created by Hugh Drummond on 6/4/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
